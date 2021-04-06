9f2b3dc60ea193efa6a6b50622fc6069f911e91d
Nathan Holm <nathan@wholm.com>
2021-04-06 04:24:26 -0700

    Minor name changes in import statement

--- src/component/user_auth/Register.jsx
+++ src/component/user_auth/Register.jsx
@@ -3,7 +3,7 @@ import validator from 'validator';
 import Form from 'react-validation/build/form';
 import Input from 'react-validation/build/input';
 import CheckButton from 'react-validation/build/button';
-import Modal from '../Modal.jsx';
+import Modal from '../Modal';
 
 import AuthService from '../../services/auth.service';
 
