a252fa80ba542bcfaea08871eaf132780c8ab8ca
Nathan Holm <nathan@wholm.com>
2021-04-06 04:30:04 -0700

    Change Main App Routes and Add Auth Logic
    - An authorized user is now presented with their user dashboard when visiting /
    - Unauthorized users are routed to the homepage
    - Navigation moved to separate component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Navigation } from './component/navigation';
-import {
-  LogIn,
-  LogOut,
-  Register,
-  Profile,
-  TestAuthorized,
-} from './component/user_auth';
+import authService from './services/auth.service';
+import { LogIn, LogOut, Register, TestAuthorized } from './component/user_auth';
 import { Home } from './component/home';
+import { AuthorizedApp } from './component/AuthorizedApp';
+import { Header } from './component/header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(undefined);
+  useEffect(() => {
+    const user = authService.getCurrentUser();
+
+    if (user) {
+      setCurrentUser(user);
+    }
+  }, []);
+
   return (
     <>
-      <Navigation />
-      <div className="container mt-3" style={{ marginTop: 40 }}>
+      <Header currentUser={currentUser} />
+      {currentUser ? (
+        <AuthorizedApp />
+      ) : (
         <main>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/register" component={Register} />
             <Route path="/log-in" component={LogIn} />
             <Route path="/log-out" component={LogOut} />
-            <Route path="/profile" component={Profile} />
             <Route path="/test-authorized" component={TestAuthorized} />
           </Switch>
         </main>
-      </div>
+      )}
     </>
   );
 }
